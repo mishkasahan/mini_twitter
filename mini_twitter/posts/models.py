c764f95dcb482b89c431ac6bd7b97443eec30a73
Alex <oleksandryanik@gmail.com>
2024-01-25 06:17:00 +0200

    create model comment

--- mini_twitter/posts/models.py
+++ mini_twitter/posts/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from mini_twitter.users.models import User
 
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
