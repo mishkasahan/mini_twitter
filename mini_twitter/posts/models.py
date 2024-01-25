874bbdb26afe0274a14ee4c36b6f3f10458d395a
Alex <oleksandryanik@gmail.com>
2024-01-25 06:10:58 +0200

    create model posts

--- mini_twitter/posts/models.py
+++ mini_twitter/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from mini_twitter.users.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
