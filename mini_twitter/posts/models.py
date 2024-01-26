69e4acfec0e51f4ef38035c235b8944106d470ea
Alex <oleksandryanik@gmail.com>
2024-01-26 02:58:59 +0200

    migrations

--- mini_twitter/posts/models.py
+++ mini_twitter/posts/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from mini_twitter.users.models import User
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
